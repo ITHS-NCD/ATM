b3b6eda58344401838521bcf7f17497af81a9666
Alfred Wendt <affewendt@gmail.com>
2026-09-14 10:12:55 +0200

    Added Michael changes

--- Program.cs
+++ Program.cs
@@ -1,149 +1,120 @@
-﻿using WestCoastBank;
+﻿using WestcoastBank;
 
 namespace ATM;
 
 class Program
 {
-    // Används vid constructors
     // static Account account = new Account("1234-5678");
+    static SavingsAccount account = new("1234-5678") { };
+    // static Account account = new("1234-5678") { };
 
-    //Använd vid objekt initiering 
-    // static Account account = new(){accountNumber = "1234-5678"};
-    static Account account = new("1234-5678"){};
-    
-
+    // static Account account = new() { accountNumber = "1234-5678" };
 
     static void Main()
     {
-        account.FirstName = "Alfred";
-        Console.WriteLine("-----------------------------------------------------");
-        Console.WriteLine("För att sätta in pengar, tryck på tangenten 'd'");
-        Console.WriteLine("För att ta ut pengar, tryck på tangenten 'w'");
-        Console.WriteLine("För att se saldo, tryck på tangenten 'b'");
-        Console.WriteLine("För att se transaktionerna, tryck på tangenten 't'");
-        Console.WriteLine("För att avsluta, tryck på tangenten 'x'");
-        Console.WriteLine("-----------------------------------------------------");
+        account.FirstName = "Michael";
+        // Här är vår enkla meny...
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("För att sätta in tryck på tangenten 'd'");
+        Console.WriteLine("För att ta ut tryck på tangenten 'w'");
+        Console.WriteLine("För att se saldo tryck på tangenten 'b'");
+        Console.WriteLine("För att se transaktionerna tryck på tangenten 't'");
+        Console.WriteLine("För att avsluta tryck på tangenten 'x'");
+        Console.WriteLine("--------------------------------------------------");
 
         App();
     }
-    
-    
+
     static void App()
     {
         try
         {
-            
             while (true)
             {
                 var key = Console.ReadLine();
-                // Console.WriteLine($"Användaren tryckte på tangenten {key}");
 
-            if (string.IsNullOrWhiteSpace(key))
-            {
-                continue;
-                throw new Exception("Du måste välja en knapp för de olika menyerna");
-            }
-
-            if (key == "x")
+                if (string.IsNullOrWhiteSpace(key) || key == "x")
                 {
-                    Console.WriteLine("Välkommen åter! Stänger ner bankomaten");
-                    Environment.Exit(0);        
+                    Environment.Exit(0);
                 }
-            
 
-            switch (key)
-            {
-                case "b":
-                    DisplayBalance();
-                    break;
-                
-                case "t":
-                    DisplayTransactions();
-                    break;
-                
-                case "d":
-                    Console.WriteLine("Hur mycket vill du sätta in?");
-                    var amount = Console.ReadLine();
+                switch (key)
+                {
+                    case "b":
+                        DisplayBalance();
+                        break;
+                    case "t":
+                        DisplayTransactions();
+                        break;
+                    case "d":
+                        Console.WriteLine("Hur mycket vill du sätta in?");
+                        var amount = Console.ReadLine();
 
-                    if (string.IsNullOrWhiteSpace(amount))
-                    {
-                        throw new Exception("Du måste ange ett heltalsvärde");
-                    }
-                    if (!int.TryParse(amount, out int result))
-                    {
-                        throw new Exception("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
-                    }
-                    Deposit(result);
-                    break;
-                
-                case "w":
-                    Console.WriteLine("Hur mycket vill du ta ut?");
-                    amount = Console.ReadLine();
-                
-                    if (string.IsNullOrWhiteSpace(amount))
-                    {
-                        throw new Exception("Du måste ange ett heltalsvärde");
-                        
-                    }
-                    if(!int.TryParse(amount, out int value))
-                    {
-                        throw new Exception("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
-                    }
-                    Withdraw(value);
-                    break;
+                        if (string.IsNullOrWhiteSpace(amount))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde");
+                        }
 
-                default:
-                    continue;
-                    throw new Exception($"Du måste välja en knapp för de olika menyerna");  
-                    
+                        if (!int.TryParse(amount, out int result))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+                        }
+                        Deposit(result);
+                        break;
+                    case "w":
+                        Console.WriteLine("Hur mycket vill du ta ut?");
+                        amount = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(amount))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde");
+                        }
+
+                        if (!int.TryParse(amount, out int value))
+                        {
+                            throw new Exception("Du måste ange ett heltalsvärde för att jag ska kunna förstå!");
+                        }
+                        WithDraw(value);
+                        break;
+                    case "x":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Ditt val finns inte i menyn");
+                        break;
+                }
             }
-                Console.WriteLine("Klicka på en knapp från menyn för att fortsätta");
-            }
-                
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
-            Environment.Exit(0);
-                    
+            App();
         }
         finally
         {
-            Console.WriteLine("Stänger ner bankomaten");
+            Console.WriteLine("Klar för idag, nu är det fredag!");
         }
     }
-   
-
-    // static void transactionsAdd(List<string> transactions = new List<string>())
-    // {
-    //     transactions.Add();
-    // }
-
-
 
     static void Deposit(int amount)
     {
-        
         account.Deposit(amount);
     }
 
-    static void Withdraw(int amount)
+    static void WithDraw(int amount)
     {
-            account.Withdraw(amount);
+        account.WithDraw(amount);
     }
-    
     static void DisplayBalance()
     {
         Console.WriteLine($"Ditt nuvarande saldo: {account.Balance}");
     }
+
     static void DisplayTransactions()
     {
-        Console.WriteLine("Dina transaktioner:");
-        foreach(var transaction in account.Transactions)
+        foreach (var tran in account.Transactions)
         {
-            Console.WriteLine(transaction.ToString());
+            Console.WriteLine(tran.ToString());
         }
     }
-
-
 }
